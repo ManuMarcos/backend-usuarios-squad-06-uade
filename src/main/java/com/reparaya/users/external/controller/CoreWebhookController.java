c276fcfa67144b32479447e599d3d039b666eac6
Agustin Grigaliunas <agustingrigaliunas@gmail.com>
2025-10-19 21:08:07 -0300

    feat: added ack and event publishing

--- src/main/java/com/reparaya/users/external/controller/CoreWebhookController.java
+++ src/main/java/com/reparaya/users/external/controller/CoreWebhookController.java
@@ -1,6 +1,7 @@
 package com.reparaya.users.external.controller;
 
 import com.reparaya.users.dto.CoreMessage;
+import com.reparaya.users.external.service.CorePublisherService;
 import com.reparaya.users.external.service.IncomingEventProcessor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,15 +19,21 @@ import org.springframework.web.bind.annotation.RestController;
 public class CoreWebhookController {
 
     private final IncomingEventProcessor incomingEventProcessor;
+    private final CorePublisherService corePublisherService;
 
     @PostMapping
     public ResponseEntity<String> handleEvent(@RequestBody CoreMessage message) {
         log.info("Event received {} from {} ", message.getDestination().getEventName(), message.getSource());
-        // send ack
-        boolean processed = incomingEventProcessor.processEventByStrategy(message);
-        if (processed) {
-            // send event to core
-            return ResponseEntity.ok("Evento " + message.getMessageId() + " procesado de forma exitosa");
+        try {
+            // TODO: chequear si esta forma de ack esta bien...
+            boolean processed = incomingEventProcessor.processEventByStrategy(message);
+            corePublisherService.sendAckToCore(message.getMessageId());
+            if (processed) {
+                // send event to core
+                return ResponseEntity.ok("Evento " + message.getMessageId() + " procesado de forma exitosa");
+            }
+        } catch (Exception ex) {
+            log.error("Unexpected error while processing event with messageId {}. Error {}", message.getMessageId(), ex.getMessage());
         }
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Ocurrió un error al procesar el evento " + message.getMessageId());
     }
--- src/main/java/com/reparaya/users/external/strategy/EventUserRegisterStrategy.java
+++ src/main/java/com/reparaya/users/external/strategy/EventUserRegisterStrategy.java
@@ -3,6 +3,8 @@ package com.reparaya.users.external.strategy;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.reparaya.users.dto.CoreMessage;
 import com.reparaya.users.dto.RegisterRequest;
+import com.reparaya.users.dto.RegisterResponse;
+import com.reparaya.users.external.service.CorePublisherService;
 import com.reparaya.users.service.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -12,12 +14,14 @@ import lombok.extern.slf4j.Slf4j;
 public class EventUserRegisterStrategy implements EventProcessStrategy {
 
     private UserService userService;
+    private CorePublisherService corePublisherService;
 
     @Override
     public boolean handle(CoreMessage event) {
         RegisterRequest request = mapRegisterRequestFromEvent(event);
         try {
-            userService.registerUser(request);
+            RegisterResponse registerResponse = userService.registerUser(request);
+            corePublisherService.sendUserCreatedToCore(registerResponse);
             return true;
         } catch (Exception ex) {
             log.error("An error ocurred while processing user registration though event with messageId {} and error {}", event.getMessageId(), ex.getMessage());
