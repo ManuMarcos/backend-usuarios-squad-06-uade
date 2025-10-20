e7a2317c688c52457e6393fe757afc93fbd8e426
Agustin Grigaliunas <agustingrigaliunas@gmail.com>
2025-10-19 22:03:46 -0300

    feat: added user reject

--- src/main/java/com/reparaya/users/external/service/CorePublisherService.java
+++ src/main/java/com/reparaya/users/external/service/CorePublisherService.java
@@ -2,6 +2,7 @@ package com.reparaya.users.external.service;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.reparaya.users.dto.CoreMessage;
 import com.reparaya.users.dto.RegisterResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,25 +20,28 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @Slf4j
 public class CorePublisherService {
-
     // TODO: store api key in env
+    public static final String API_KEY = "ch_9aa39636744843d880a69e45cd08f1ab";
 
-    private final String SUBSCRIPTION_ID = "ce687bc8-7b6b-4a83-a34c-94c31114476e";
-    private final String CORE_ACK_URL = "";
+    // TODO: manejar subscription id por cada topico
+    private final String PRESTADORES_ALTA_SUBSCRIPTION_ID = "ce687bc8-7b6b-4a83-a34c-94c31114476e";
+
+    private final String CORE_ACK_URL = "https://nonprodapi.uade-corehub.com/messages/";
     private final String CORE_EVENT_PUBLISH_URL = "https://nonprodapi.uade-corehub.com/publish";
 
     public void sendAckToCore(final String messageId) {
         RestTemplate rt = new RestTemplate();
-        String url = "https://nonprodapi.uade-corehub.com/messages/" + messageId + "/ack";
+        String url = CORE_ACK_URL + messageId + "/ack";
 
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add("x-api-key", "ch_9aa39636744843d880a69e45cd08f1ab");
+        headers.add("x-api-key", API_KEY);
 
-        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(Map.of("msgId", messageId, "subscriptionId", SUBSCRIPTION_ID), headers);
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(Map.of("msgId", messageId, "subscriptionId", PRESTADORES_ALTA_SUBSCRIPTION_ID), headers);
         log.info("Sending ACK for messageId {}", messageId);
 
-        // TODO: uncomment
+        // TODO: comentado por ahora. Ver que pasa con el subscription id.
+
         //String response = rt.postForObject(url, entity, String.class);
         //log.info("Received ACK response {} from core for messageId {}", response, messageId);
     }
@@ -48,7 +52,7 @@ public class CorePublisherService {
         RestTemplate rt = new RestTemplate();
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
-        headers.add("x-api-key", "ch_9aa39636744843d880a69e45cd08f1ab");
+        headers.add("x-api-key", API_KEY);
 
         Map<String, Object> userData = Map.of(
                 "message", registerResponse.getMessage(),
@@ -79,5 +83,35 @@ public class CorePublisherService {
 
     public void sendUserUpdatedToCore(final String messageId) {
         // implement
+    }
+
+    public void sendUserRejectedToCore(final CoreMessage message, final String errorMessage) {
+        UUID messageId = UUID.randomUUID();
+
+        RestTemplate rt = new RestTemplate();
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("x-api-key", API_KEY);
+
+        Map<String, Object> payload = Map.of(
+                "message", "Usuario rechazado. Motivo: " + errorMessage,
+                "email", message.getPayload().get("email")
+        );
+
+        Map<String, Object> body = Map.of(
+                "messageId", messageId,
+                "timestamp", OffsetDateTime.now().toString(),
+                "source", "users",
+                "destination", Map.of(
+                        "channel", "users.user.user_rejected",
+                        "eventName", "user_rejected"
+                ),
+                "payload", payload
+        );
+        HttpEntity<Map<String, Object>> entity = new HttpEntity<>(body, headers);
+        log.info("Sending user rejected event with messageId {}", messageId);
+
+        String response = rt.postForObject(CORE_EVENT_PUBLISH_URL, entity, String.class);
+        log.info("Received user rejected response from core {} for messageId {}", response, messageId);
     }
 }
