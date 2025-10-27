a37e60d5397bc5cb762e54abc770158f38753c30
Agustin Hess <108557104+Hessoide@users.noreply.github.com>
2025-10-26 21:11:55 -0300

    Modificaciones extras para S3
    Cambios en S3 storage para dejarlo mas prolijo

--- src/main/java/com/reparaya/users/service/S3StorageService.java
+++ src/main/java/com/reparaya/users/service/S3StorageService.java
@@ -1,8 +1,8 @@
 package com.reparaya.users.service;
 
+import com.reparaya.users.controller.FileController;
 import com.reparaya.users.dto.PresignUploadReq;
-import jakarta.annotation.PostConstruct;
-import lombok.extern.slf4j.Slf4j;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
@@ -13,70 +13,81 @@ import software.amazon.awssdk.services.s3.model.PutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.S3Presigner;
 import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
 import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+import jakarta.annotation.PostConstruct;
+
 
 import java.net.URL;
 import java.time.Duration;
 import java.util.UUID;
 
-@Slf4j
 @Service
+@RequiredArgsConstructor
 public class S3StorageService {
 
-    @Value("${aws.s3.access-key}")
+
+
+    @Value("${aws.s3.accessKey}")
     private String accessKey;
 
-    @Value("${aws.s3.secret-key}")
+    @Value("${aws.s3.secretKey}")
     private String secretKey;
 
     @Value("${aws.s3.bucket}")
     private String bucket;
 
-    private final String region = "us-east-2";
+    @Value("${aws.s3.region}")
+    private String region;
 
     private S3Presigner presigner;
-    private S3Client s3;
 
     @PostConstruct
-    private void init() {
-        try {
-            AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
-
-            this.presigner = S3Presigner.builder()
-                    .region(Region.of(region))
-                    .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-                    .build();
-
-            this.s3 = S3Client.builder()
-                    .region(Region.of(region))
-                    .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
-                    .build();
-
-            log.info("S3 client inicializado correctamente con región {}", region);
-        } catch (Exception e) {
-            log.error("Error inicializando S3StorageService: {}", e.getMessage());
-            throw new IllegalStateException("No se pudo inicializar S3StorageService", e);
+    void init() {
+        if (accessKey == null || accessKey.isBlank() || secretKey == null || secretKey.isBlank()) {
+            throw new IllegalStateException("AWS credentials no configuradas. Revisá AWS_ACCESS_KEY_ID y AWS_SECRET_ACCESS_KEY.");
         }
-    }
 
-    public URL generateUploadPresignedUrl(String objectKey, String contentType, int minutes) {
-        PutObjectRequest put = PutObjectRequest.builder()
-                .bucket(bucket)
-                .key(objectKey)
-                .contentType(contentType)
+        AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey, secretKey);
+
+        this.presigner = S3Presigner.builder()
+                .region(Region.US_EAST_2) // región fija como pediste
+                .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
                 .build();
 
-        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
-                .signatureDuration(Duration.ofMinutes(minutes))
-                .putObjectRequest(put)
-                .build();
 
-        PresignedPutObjectRequest presigned = presigner.presignPutObject(presignRequest);
-        return presigned.url();
+//        this.s3 = S3Client.builder()
+//        .region(Region.US_EAST_2)
+//        .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+//        .build();
     }
+//    AwsBasicCredentials awsCreds = AwsBasicCredentials.create(accessKey,secretKey);
+//
+//    private final S3Presigner presigner = S3Presigner.builder()
+//            .region(Region.US_EAST_2)
+//            .credentialsProvider(StaticCredentialsProvider.create(awsCreds))
+//            .build();
+//
+//    private final S3Client s3;
 
-    public String buildPublicUrl(String objectKey) {
-        return "https://" + bucket + ".s3." + region + ".amazonaws.com/" + objectKey;
-    }
+
+//    public URL generateUploadPresignedUrl(String objectKey, String contentType, int minutes) {
+//        PutObjectRequest put = PutObjectRequest.builder()
+//                .bucket(bucket)
+//                .key(objectKey)
+//                .contentType(contentType)
+//                .build();
+//
+//        PutObjectPresignRequest presignRequest = PutObjectPresignRequest.builder()
+//                .signatureDuration(Duration.ofMinutes(minutes))
+//                .putObjectRequest(put)
+//                .build();
+//
+//        PresignedPutObjectRequest presigned = presigner.presignPutObject(presignRequest);
+//        return presigned.url();
+//    }
+//
+//    public String buildPublicUrl(String objectKey) {
+//        return "https://" + bucket + ".s3." + region + ".amazonaws.com/" + objectKey;
+//    }
 
     public String presignedURL(PresignUploadReq req) {
         UUID uuid = UUID.randomUUID();
@@ -84,6 +95,7 @@ public class S3StorageService {
                 .bucket(bucket)
                 .key(uuid.toString())
                 .build();
+
 
         PresignedPutObjectRequest presignedRequest = presigner.presignPutObject(
                 r -> r.signatureDuration(Duration.ofMinutes(10))
