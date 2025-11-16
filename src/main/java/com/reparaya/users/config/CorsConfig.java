00c5495fef4d461a9d535a6278fd51d0aa56db6a
Manuel Marcos <38569631+ManuMarcos@users.noreply.github.com>
2025-11-16 10:24:33 -0300

    Update CORS configuration to allow all origins

--- src/main/java/com/reparaya/users/config/CorsConfig.java
+++ src/main/java/com/reparaya/users/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://arreglaya-usuarios.vercel.app/", "https://dev-arreglaya-usuarios.vercel.app/")
+                        .allowedOrigins("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*");
             }
